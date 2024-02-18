f4cd23f088b98780439f4a81d9337212f8a1abea
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-18 00:47:21 +0000

    Navbar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,46 @@
+import { NavLink } from "react-router-dom";
 import "./header.css";
 
 const Header = () => {
   return (
     <header className="header ">
-      <div className="content">Header</div>
+      <div className="content">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink
+              to="projects"
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="contact"
+              end
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
