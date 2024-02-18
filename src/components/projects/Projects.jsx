f601b78432f10d32862dd5d6d9708a596dd30359
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-18 22:06:37 +0000

    imported projects data in projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,22 +1,10 @@
-import { Link, Route, Routes } from "react-router-dom";
 import "./projects.css";
-import ProjectOne from "./projectOne/ProjectOne";
-import ProjectTwo from "./projectTwo/ProjectTwo";
+import projectsData from "../../projects.json";
 
 const Projects = () => {
   return (
     <main className="projects main-section">
       <h1>Projects</h1>
-      <Link to="project-one" role="button" className="btn btn-link">
-        Project One
-      </Link>
-      <Link to="project-two" role="button" className="btn btn-link">
-        Project Two
-      </Link>
-      <Routes>
-        <Route path="project-one" element={<ProjectOne />} />
-        <Route path="project-two" element={<ProjectTwo />} />
-      </Routes>
     </main>
   );
 };
