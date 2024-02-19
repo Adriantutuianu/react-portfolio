5916042db32838cd08c246a4764893246fe4b79a
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-19 02:11:13 +0000

    Styled footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import "./footer.css";
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="footer ">
-      <div className="content"> footer</div>
+    <footer>
+      <div className="footer-content">
+        <a href="https://adriantutuianu.github.io/react-portfolio/">
+          &copy; {currentYear} Adrian Tut Portfolio
+        </a>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
