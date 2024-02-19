ea92f27fe01cdbe293cd68b68df226de3624f62b
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-19 22:02:58 +0000

    Added text to home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,17 @@
 import "./home.css";
 const Home = () => {
-  return <main className="home main-section">Home</main>;
+  return (
+    <main className="home main-section">
+      <div>
+        <h1 className="title">HI THERE! I'M ADRIAN</h1>
+        <h2 className="sub-title">
+          A Front-End Web Developer passionate about creating interactive
+          applications and experiences on the web.
+        </h2>
+        <h3>Take a look at my projects!</h3>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
