55117f5c4ef3d8646ddc1dfad7345e844d40456e
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-19 22:41:52 +0000

    Finished style home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,23 @@
 import "./home.css";
 import Lottie from "lottie-react";
 import animationHome from "../../Animation - home.json";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <main className="home main-section">
       <div className="home-text">
         <h2 className="title">
-          Hi there! I'm <span className="text-span">Adrian!</span>
+          Hi there! I'm <span className="title-span">Adrian!</span>
         </h2>
         <h3 className="sub-title">
           A Front-End Web Developer passionate about creating interactive
           applications and experiences on the web.
         </h3>
-        <h3 className="text-span">Take a look at my projects!</h3>
+
+        <NavLink to="projects" end className="text-span">
+          Take a look at my projects!
+        </NavLink>
       </div>
       <Lottie
         className="lottie-home"
