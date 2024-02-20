53676a6418deb4466d726621f9400675fcb88d6a
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-20 13:18:47 +0000

    Added projects in projects.json

--- src/components/projects/project/Project.jsx
+++ src/components/projects/project/Project.jsx
@@ -5,11 +5,11 @@ const Project = ({ project }) => {
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-lg-6 col-md-12 col-sm-12 p-3">
+        <div className="col-lg-8 col-md-10 col-sm-12 p-3">
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">{project.title}</h5>
-              <p className="card-text">Description: {project.description}</p>
+              <p className="card-text">{project.shortDescription}</p>
               <Link
                 to={project.path}
                 role="button"
