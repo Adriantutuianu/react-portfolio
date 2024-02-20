2111978c55e2c43b2c5b155101729d98c69d056a
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-20 00:53:59 +0000

    Finished notFound project page

--- src/components/not-found/NotFound.jsx
+++ src/components/not-found/NotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Lottie from "lottie-react";
+import notFound from "../../icons/Animation - notFound.json";
+import "./notFound.css";
 const NotFound = () => {
-  return <main className="projects main-section">Not Found Page...</main>;
+  return (
+    <main className="projects-notFound main-section">
+      {" "}
+      <Lottie
+        className="lottie-notFound"
+        animationData={notFound}
+        loop={true}
+      />
+    </main>
+  );
 
   //TODO: link to homepage(button -return to homepage)
 };
