607647f80db3c39b7b45f97117b17305421a27e4
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-20 02:44:31 +0000

    Project details content/path

--- src/components/projects/project-details/ProjectDetails.jsx
+++ src/components/projects/project-details/ProjectDetails.jsx
@@ -1,6 +1,8 @@
 import projectsData from "../../../projects.json";
 import { useLocation } from "react-router-dom";
 import NotFound from "../../not-found/NotFound";
+import { Link } from "react-router-dom";
+import "./projectDetails.css";
 
 const ProjectDetails = () => {
   let location = useLocation();
@@ -9,8 +11,18 @@ const ProjectDetails = () => {
   const data = projectsData.find((proj) => proj.path === locationPath);
   console.log("DATA: ", data);
   return data ? (
-    <main className="projects main-section">
-      <p>{data?.title}</p>
+    <main className="project-details main-section">
+      <img src={data.screenshot} className="img-screenshot" alt={data.title} />
+      <h1>{data?.title}</h1>
+      <section>{data.description}</section>
+      <p>Deployed link:</p>
+      <Link to={data.deployedLink} role="button" className="btn btn-link">
+        This is the link to deployed application.
+      </Link>
+      <p>Github Link: </p>
+      <Link to={data.githubLink} role="button" className="btn btn-link">
+        This is the link to Github Repository.
+      </Link>
     </main>
   ) : (
     <NotFound />
