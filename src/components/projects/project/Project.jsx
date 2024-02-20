0ff9193c55875eccdc4ee12a06f5726da0a3b171
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-20 11:53:37 +0000

    Styled projects

--- src/components/projects/project/Project.jsx
+++ src/components/projects/project/Project.jsx
@@ -3,39 +3,22 @@ import "./project.css";
 
 const Project = ({ project }) => {
   return (
-    <div className="col-lg-6 col-md-12 col-sm-12 mb-4">
-      <div className="card">
-        {/* <img
-          src={project.screenshot}
-          className="card-img-top"
-          alt={project.title}
-        /> */}
-        <div className="card-body">
-          <h5 className="card-title">{project.title}</h5>
-          <p className="card-text">Description: {project.description}</p>
-          {/* <a
-            href={project.deployedLink}
-            className="btn btn-primary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deployed Version
-          </a>
-          <a
-            href={project.githubLink}
-            className="btn btn-secondary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub Repository
-          </a> */}
-          <Link
-            to={project.path}
-            role="button"
-            className="btn btn-secondary btn-link"
-          >
-            Project details
-          </Link>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-6 col-md-12 col-sm-12 p-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{project.title}</h5>
+              <p className="card-text">Description: {project.description}</p>
+              <Link
+                to={project.path}
+                role="button"
+                className="btn btn-secondary btn-link btn-style"
+              >
+                Project details
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
