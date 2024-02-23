2d65486e04853c1f6afcdafce0390564dc88d2ce
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-23 19:56:17 +0000

    Update contact section- deleted form contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,36 +4,9 @@ import lottieCard from "../../icons/Animation - contact.json";
 import MailImage from "../../icons/email img.png";
 import LinkedInImage from "../../icons/linkedin.png";
 import GithubImage from "../../icons/github img.png";
-import { Form, Button } from "react-bootstrap";
 import "./contact.css";
 
 const Contact = () => {
-  // State for form data
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  // Handle form input change
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-
-    // Reset form data after submission
-    setFormData({
-      name: "",
-      email: "",
-      message: "",
-    });
-  };
-
   // Render Contact component
   return (
     <main className="contact main-section">
@@ -76,53 +49,6 @@ const Contact = () => {
           <span>/adriantutuianu</span>
         </a>
       </div>
-      <Form className="contact-form" onSubmit={handleSubmit}>
-        <Form.Group controlId="formName">
-          <Form.Control
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            value={formData.name}
-            onChange={handleChange}
-            size="sm"
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label></Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            value={formData.email}
-            onChange={handleChange}
-            size="sm"
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formMessage">
-          <Form.Label></Form.Label>
-          <Form.Control
-            as="textarea"
-            name="message"
-            rows={3}
-            placeholder="Enter your message"
-            value={formData.message}
-            onChange={handleChange}
-            size="sm"
-            required
-          />
-        </Form.Group>
-        <Button
-          className="btn-form"
-          variant="secondary"
-          type="submit"
-          size="sm"
-        >
-          {" "}
-          Send!
-        </Button>
-      </Form>
       <Lottie
         className="lottie-contact-letter"
         animationData={lottieCard}
